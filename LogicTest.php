13d274e062470449c21d06265d7f0491460ce4e6
Michael Velasco <maicolvelasco55@gmail.com>
2021-08-07 15:18:23 -0500

    Punto 1, suma de múltiplos dentro de un rango

--- LogicTest.php
+++ LogicTest.php
@@ -2,4 +2,24 @@
 
 class LogicTest{
 
+    /**
+     * Suma de múltiplos dentro de un rango límite
+     *
+     * @param integer $limit
+     * @param integer ...$multiples
+     * @return int
+     */
+    public static function sumOfMultiples(int $limit, int ...$multiples){
+        if(count($multiples) <= 0){
+            return 0;
+        }
+
+        $result = 0;
+        $number = array_shift($multiples);
+
+        for ($i=1; $i <= intdiv(($limit-1), $number); $i++) { 
+            $result += $number*$i;
+        }
+        return $result + LogicTest::sumOfMultiples($limit, ...$multiples);
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
 include './LogicTest.php';
+
+echo LogicTest::sumOfMultiples(10, 3,5); // 23
